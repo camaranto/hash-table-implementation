a7eebe3d6bd3fbfbf4732c04fb90dce3ef69eb09
camaranto <camaranto@uninorte.edu.co>
2024-12-07 19:35:20 -0500

    Hotfix: Change function type

--- src/hash_table.c
+++ src/hash_table.c
@@ -84,7 +84,7 @@ void ht_insert(ht_hash_table* ht, const char* key, const char* value) {
     ht->count++;
 }
 
-void ht_search(ht_hash_table* ht, const char* key) {
+char* ht_search(ht_hash_table* ht, const char* key) {
     int index = ht_get_hash(key, ht->size, 0);
     ht_item* item = ht->items[index];
     int i = 1;
